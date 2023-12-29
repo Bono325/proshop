4994b38c60fff3f3b46793fb289533051777e585
Bono325 <sibono.mbona@gmail.com>
2023-12-29 17:12:27 +0100

    MongoDB and Mongoose setup

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        defualt: false,
+    },
+}, {
+    timestamps: true,
+});
+
+const User = mongoose.model("Üser", userSchema);
+
+export default User;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import { Container } from "react-bootstrap"
+import { Outlet } from "react-router-dom"
+import Header from "./componets/Header"
+import Footer from "./componets/Footer"
+
 
 const App = () => {
   return (
-    <h1>\\welcome to \Proshop</h1>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+           <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
