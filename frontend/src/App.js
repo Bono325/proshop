7d0719b3eab0c39f6bd915fe173e35343e9cf855
Bono325 <sibono.mbona@gmail.com>
2024-01-03 05:46:05 +0100

    section 8 done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import { Container } from "react-bootstrap"
 import { Outlet } from "react-router-dom"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./componets/Header"
 import Footer from "./componets/Footer"
-
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         </Container>
       </main>
       <Footer />
+      <ToastContainer />
     </>
   )
 }
